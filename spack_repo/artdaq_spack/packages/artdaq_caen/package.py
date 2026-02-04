92dbe84d9533c6400cf948f47571896c75915bd9
Eric Flumerfelt <eflumerf@fnal.gov>
2026-02-04 14:25:35 -0600

    Add package configuration for artdaq_caen

---
+++ spack_repo/artdaq_spack/packages/artdaq_caen/package.py
@@ -0,0 +1,79 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+import sys
+
+from spack.package import *
+
+
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+
+class ArtdaqCaen(CMakePackage):
+    """The toolkit currently provides functionality for data transfer,
+    event building, event reconstruction and analysis (using the art analysis
+    framework), process management, system and process state behavior, control
+    messaging, local message logging (status and error messages), DAQ process
+    and art module configuration, and the writing of event data to disk in ROOT
+    format."""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/artdaq/wiki"
+    url = "https://github.com/art-daq/artdaq-caen/archive/refs/tags/v1_00_00.tar.gz"
+    git = "https://github.com/art-daq/artdaq-caen.git"
+
+    version("develop", branch="develop", get_full_repo=True)
+
+    def url_for_version(self, version):
+        url = "https://github.com/art-daq/artdaq-caen/archive/refs/tags/{0}.tar.gz"
+        return url.format(version)
+
+    variant(
+        "cxxstd",
+        default="20",
+        values=("17", "20"),
+        multi=False,
+        sticky=True,
+        description="Use the specified C++ standard when building.",
+    )
+
+    depends_on("cetmodules@3.26.00:", type="build")
+
+    depends_on("artdaq@v4_00_00:,develop", when="@v4_00_00:,develop")
+    depends_on("art-suite")
+
+    depends_on("caencomm")
+    depends_on("caenvmelib")
+    depends_on("caendigitizer")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd"),
+        ]
+        if os.path.exists("CMakePresets.cmake"):
+            args.extend(["--preset", "default"])
+        else:
+            self.define("artdaq_caen_OLD_STYLE_CONFIG_VARS", True)
+        return args
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
