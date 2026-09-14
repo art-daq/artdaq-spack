bea1e39cb9171643c90dbb22bcb522bbcec9406e
Eric Flumerfelt <eflumerf@fnal.gov>
2026-09-14 08:02:31 -0500

    Run py-black to format

--- packages/py-psycopg2/package.py
+++ packages/py-psycopg2/package.py
@@ -11,17 +11,46 @@ class PyPsycopg2(PythonPackage):
     homepage = "https://psycopg.org/"
     pypi = "psycopg2/psycopg2-2.8.6.tar.gz"
 
-
-    version("2.9.13", sha256="d36784fc2dae69523ba4b79c7d1d1b4d6e83e87836874f111262f4db940b16a6")
-    version("2.9.12", sha256="1dedb1c7a1d8552c4a6044c6b1c41a52e6a8e2d144af83eccac758076b1b7c15")
-    version("2.9.11", sha256="964d31caf728e217c697ff77ea69c2ba0865fa41ec20bb00f0977e62fdcc52e3")
-    version("2.9.10", sha256="12ec0b40b0273f95296233e8750441339298e6a572f7039da5b260e3c8b60e11")
-    version("2.9.9", sha256="d1454bde93fb1e224166811694d600e746430c006fbb031ea06ecc2ea41bf156")
-    version("2.9.8", sha256="3da6488042a53b50933244085f3f91803f1b7271f970f3e5536efa69314f6a49")
-    version("2.9.7", sha256="f00cc35bd7119f1fed17b85bd1007855194dde2cbd8de01ab8ebb17487440ad8")
-    version("2.9.6", sha256="f15158418fd826831b28585e2ab48ed8df2d0d98f502a2b4fe619e7d5ca29011")
-    version("2.9.1", sha256="de5303a6f1d0a7a34b9d40e4d3bef684ccc44a49bbe3eb85e3c0bffb4a131b7c")
-    version("2.8.6", sha256="fb23f6c71107c37fd667cb4ea363ddeb936b348bbd6449278eb92c189699f543")
+    version(
+        "2.9.13",
+        sha256="d36784fc2dae69523ba4b79c7d1d1b4d6e83e87836874f111262f4db940b16a6",
+    )
+    version(
+        "2.9.12",
+        sha256="1dedb1c7a1d8552c4a6044c6b1c41a52e6a8e2d144af83eccac758076b1b7c15",
+    )
+    version(
+        "2.9.11",
+        sha256="964d31caf728e217c697ff77ea69c2ba0865fa41ec20bb00f0977e62fdcc52e3",
+    )
+    version(
+        "2.9.10",
+        sha256="12ec0b40b0273f95296233e8750441339298e6a572f7039da5b260e3c8b60e11",
+    )
+    version(
+        "2.9.9",
+        sha256="d1454bde93fb1e224166811694d600e746430c006fbb031ea06ecc2ea41bf156",
+    )
+    version(
+        "2.9.8",
+        sha256="3da6488042a53b50933244085f3f91803f1b7271f970f3e5536efa69314f6a49",
+    )
+    version(
+        "2.9.7",
+        sha256="f00cc35bd7119f1fed17b85bd1007855194dde2cbd8de01ab8ebb17487440ad8",
+    )
+    version(
+        "2.9.6",
+        sha256="f15158418fd826831b28585e2ab48ed8df2d0d98f502a2b4fe619e7d5ca29011",
+    )
+    version(
+        "2.9.1",
+        sha256="de5303a6f1d0a7a34b9d40e4d3bef684ccc44a49bbe3eb85e3c0bffb4a131b7c",
+    )
+    version(
+        "2.8.6",
+        sha256="fb23f6c71107c37fd667cb4ea363ddeb936b348bbd6449278eb92c189699f543",
+    )
 
     depends_on("c", type="build")  # generated
 
